e288dd6d96c3125671b1065b79c6b9b3d89f6d80
David Ngassa <ngassadavid97@gmail.com>
2021-01-07 14:29:55 +0400

    :rocket: Getting data from a server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,111 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs'
+import {catchError, map, tap} from 'rxjs/operators'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+
 import {MessageService} from './message.service'
 import {Hero} from './hero'
-import {HEROES} from './mock-heroes'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes():Observable<Hero[]>{
-    // TODO: send the message after fetching the heroes
-    this.messageService.add('HeroService: fetched heroes')
-    return of (HEROES)
+  private heroesUrl = 'api/heroes' //URL to web api
+
+  httpOptions ={
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
   }
 
+  /** GET heroes from the server */
+  getHeroes():Observable<Hero[]>{   
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
   getHero(id:number): Observable<Hero>{
-    //TODO: send the message after fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`)
-    return of(HEROES.find(hero => hero.id === id)!)
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  } 
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim())
+    {
+      //if not search term, return empty hero array
+      return of([])
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? 
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+
+    )
   }
 
-  constructor(private messageService: MessageService) { }
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
+  /** PUT: update the hero on the server */
+
+  updateHero(hero :Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }  
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof hero === 'number' ? hero : hero.id
+    const url = `${this.heroesUrl}/${id}`
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    )
+  }
+  
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error:any): Observable<T>=>{
+      //TODO: send the error to remote loggin infrastructure
+      console.error(error) // log to console instead
+      
+      //TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed : ${error.message}`)
+
+      // Let the app keep running by returning an empty result
+      return of(result as T)
+    }
+  }
+
+   /** Log a HeroService message with the MessageService */
+   private log(message: string){
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 }
